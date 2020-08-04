1552e32bba434dda415e0d8b622d711eb8d67e32
huilinghe19 <huiling.he@helmholtz-berlin.de>
2020-08-04 14:18:29 +0200

    add devices in sardana with a script

--- addDeviceinSardana.py
+++ addDeviceinSardana.py
@@ -7,11 +7,11 @@ MaxDevice = 8
 DeviceList=[]
 for i in range(5, MaxDevice+1):
     deviceName='motor/simctrl/'+str(i)
-    DeviceList.append(deviceName)
+    DeviceList.append([i, deviceName])
 print(DeviceList)
-for i in DeviceList:
-    dev_info.name = i
+for i, deviceName in DeviceList:
+    dev_info.name = deviceName
     dev_info._class = 'Motor'
     dev_info.server = 'Sardana/simulation'
     db.add_device(dev_info)
-    db.put_device_property(dev_info.name, {"ctrl_id": "5"})
+    db.put_device_property(dev_info.name, {"axis": i, "ctrl_id": "5", "id": i+5})
