9ef1f9c1ef711b830b9916eed247b8a56c9c7820
huilinghe19 <huiling.he@helmholtz-berlin.de>
2020-08-20 15:15:33 +0200

    check motor state in spock

--- programs/salute.py
+++ programs/salute.py
@@ -8,53 +8,6 @@ from sardana.macroserver.msexception import StopException
 from epics import Motor
 
 
-@macro()
-def hello_world(self):
-    self.output('Running hello_world...')
-
-
-class HelloWorld(Macro):
-    """Hello, World! macro"""
-
-    def run(self):
-        self.output("Hello, World!")
-
-
-
-
-@macro()
-def hello_world2(self):
-    """This is an hello world macro"""
-    print("Hello, World!")
-
-
-from sardana import State
-
-
-@macro([["motor", Type.Motor, None, "Motor to oscilate"],
-        ["amplitude", Type.Float, None, "Oscilation amplitude"],
-        ["integ_time", Type.Float, None, "Integration time"]])
-def oscillate(self, motor, amplitude, integ_time):
-    """Acquire with the active measurement group while oscillating a motor.
-    """
-    motion = self.getMotion([motor])
-    curr_pos = motor.getPosition()
-    positions = [curr_pos + amplitude / 2,
-                 curr_pos - amplitude / 2]
-
-    mnt_grp_name = self.getEnv("ActiveMntGrp")
-    mnt_grp = self.getMeasurementGroup(mnt_grp_name)
-    mnt_grp.putIntegrationTime(integ_time)
-
-    i = 0
-    id_ = mnt_grp.startCount()
-    while mnt_grp.State() == State.Moving:
-        motion.move(positions[i])
-        i += 1
-        i %= 2
-    mnt_grp.waitCount(id_)
-
-#oscillate('top', '20', '10')
 
 @macro([["motorName", Type.String, None, "Epics Motor Name"]])
 def check_motor_state(self, motorName):
@@ -83,7 +36,6 @@ def check_motor_state(self, motorName):
     
     self.output("Epics motor {} state: {}".format(motorName, stateID))
     stateID_binary_conversion = bin(int(stateID))
-    #print(stateID_binary_conversion))
     #self.output(str(stateID_binary_conversion))
     state_conversion = str(stateID_binary_conversion)[2:]
     #self.output(str(state_conversion))
@@ -95,8 +47,7 @@ def check_motor_state(self, motorName):
         count += 1
         self.output('Bit {}: {} >>> {}'.format(count, each_char, list_result[count]))
         if each_char == str(1):
-            list_state1.append(count)
-            
+            list_state1.append(count)            
     #self.output(list_state1)
 
     self.output("\nProblem Diagnosis:\n  ")
@@ -105,10 +56,54 @@ def check_motor_state(self, motorName):
             self.output("Bit {}: 1  >>>  {} ".format(i, list_result[i]))
     else:
         self.output("None")        
-    #if stateID == 16802:
-        #self.output("HOMED: the motor has been homed.")
-    ## print function will be shown in sardana server.
-    #print(epics_motor.get("MSTA"))
+ 
+
+
+@macro()
+def hello_world(self):
+    self.output('Running hello_world...')
+
+
+class HelloWorld(Macro):
+    """Hello, World! macro"""
+
+    def run(self):
+        self.output("Hello, World!")
+
+
+@macro()
+def hello_world2(self):
+    """This is an hello world macro"""
+    print("Hello, World!")
+
+
+from sardana import State
+
+@macro([["motor", Type.Motor, None, "Motor to oscilate"],
+        ["amplitude", Type.Float, None, "Oscilation amplitude"],
+        ["integ_time", Type.Float, None, "Integration time"]])
+def oscillate(self, motor, amplitude, integ_time):
+    """Acquire with the active measurement group while oscillating a motor.
+    """
+    motion = self.getMotion([motor])
+    curr_pos = motor.getPosition()
+    positions = [curr_pos + amplitude / 2,
+                 curr_pos - amplitude / 2]
+
+    mnt_grp_name = self.getEnv("ActiveMntGrp")
+    mnt_grp = self.getMeasurementGroup(mnt_grp_name)
+    mnt_grp.putIntegrationTime(integ_time)
+
+    i = 0
+    id_ = mnt_grp.startCount()
+    while mnt_grp.State() == State.Moving:
+        motion.move(positions[i])
+        i += 1
+        i %= 2
+    mnt_grp.waitCount(id_)
+
+
+ 
 
 
 
